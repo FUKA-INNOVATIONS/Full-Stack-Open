8c1ee62e232cefacc35a477ab4b0694d67290541
fuad.kalhori@gmail.com <fuad.kalhori@gmail.com>
2021-10-15 18:33:32 +0300

    done: 2.9*: puhelinluettelo step4

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -6,16 +6,36 @@ const App = () => {
     { name: 'Arto Hellas', phone: '041 1234567' },
     { name: 'Fuad Kalhori', phone: '020 12121212' },
     { name: 'Matti Luukkanen', phone: '045 1235432' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' },
   ] );
 
   const [ newName, setNewName ] = useState( '' );
   const [ newPhone, setNewPhone ] = useState( '' );
 
+  const [ nameFilter, setNameFilter ] = useState( '' );
+  const [ showAll, setShowAll ] = useState( true );
+
+  const personsToShow = showAll ? persons : persons.filter(person => person.name.includes(nameFilter));
+
+  const handleNameFilterChange = e => {
+
+    let filterTerm = e.target.value;
+    if ( filterTerm.length > 0)  {
+      setShowAll(false)
+    }else {
+      setShowAll(true)
+    }
+
+    setNameFilter( filterTerm );
+  }
+
   // Add name input value to newName state
   const handleNameChange = e => setNewName( e.target.value );
 
   // Add New phone input value to state
-  const handlePhoneChange =  e => setNewPhone(e.target.value)
+  const handlePhoneChange = e => setNewPhone( e.target.value );
 
   // Add new person to the list
   const handleAddPerson = ( e ) => {
@@ -44,21 +64,26 @@ const App = () => {
   return (
       <div>
         <h2>Phonebook</h2>
+
+        <div>Filter shown with: <input value={ nameFilter }
+                                       onChange={ handleNameFilterChange }/>
+        </div>
+
+        <h2>Add new</h2>
         <form onSubmit={ handleAddPerson }>
           <div>
             name: <input value={ newName } onChange={ handleNameChange }/>
           </div>
           <div>
-            phone: <input value={newPhone} onChange={handlePhoneChange} />
+            phone: <input value={ newPhone } onChange={ handlePhoneChange }/>
           </div>
           <div>
             <button type="submit">add</button>
           </div>
         </form>
         <h2>Numbers</h2>
-        { persons.map( person =>
-            <p key={ person.name }>{ person.name } ({ person.phone })</p>,
-        ) }
+        { personsToShow.map( person => <p
+            key={ person.name }>{ person.name } ({ person.phone })</p> ) }
       </div>
   );
 
