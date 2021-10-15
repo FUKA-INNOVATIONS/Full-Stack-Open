f61e234742f89b135882dab11a9a5bf1ba40cf56
fuad.kalhori@gmail.com <fuad.kalhori@gmail.com>
2021-10-15 17:57:19 +0300

    done: 2.8: puhelinluettelo step3

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,46 +1,55 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [ persons, setPersons ] = useState( [
-    { name: 'Arto Hellas' },
-    {name: 'Fuad Kalhori'},
-  ] );
-  const [ newName, setNewName ] = useState( '' );
 
-  // Add input value to newName state
-  const handleNameChange = ( e ) => {
-    setNewName( e.target.value );
-  };
+  const [ persons, setPersons ] = useState( [
+    { name: 'Arto Hellas', phone: '041 1234567' },
+    { name: 'Fuad Kalhori', phone: '020 12121212' },
+    { name: 'Matti Luukkanen', phone: '045 1235432' },
+  ] );
+
+  const [ newName, setNewName ] = useState( '' );
+  const [ newPhone, setNewPhone ] = useState( '' );
+
+  // Add name input value to newName state
+  const handleNameChange = e => setNewName( e.target.value );
+
+  // Add New phone input value to state
+  const handlePhoneChange =  e => setNewPhone(e.target.value)
 
   // Add new person to the list
-  const handleAddName = ( e ) => {
+  const handleAddPerson = ( e ) => {
     e.preventDefault();
 
     let personExists = false;
 
     // Alert incase name already exists
-    persons.forEach(person => {
-      if(person.name.toLowerCase() === newName.toLocaleLowerCase()){
+    persons.forEach( person => {
+      if ( person.name.toLowerCase() === newName.toLocaleLowerCase() ) {
         personExists = true;
-        alert(`${newName} is already added to  phonebook`)
+        alert( `${ newName } is already added to  phonebook` );
       }
-    })
+    } );
 
     // Add new person to phonebook
-    if (!personExists) {
+    if ( !personExists ) {
       const newPerson = {
         name: newName,
+        phone: newPhone,
       };
-      setPersons( persons.concat(newPerson) );
+      setPersons( persons.concat( newPerson ) );
     }
   };
 
   return (
       <div>
         <h2>Phonebook</h2>
-        <form onSubmit={ handleAddName }>
+        <form onSubmit={ handleAddPerson }>
           <div>
             name: <input value={ newName } onChange={ handleNameChange }/>
+          </div>
+          <div>
+            phone: <input value={newPhone} onChange={handlePhoneChange} />
           </div>
           <div>
             <button type="submit">add</button>
@@ -48,7 +57,7 @@ const App = () => {
         </form>
         <h2>Numbers</h2>
         { persons.map( person =>
-            <p key={person.name}>{ person.name }</p>,
+            <p key={ person.name }>{ person.name } ({ person.phone })</p>,
         ) }
       </div>
   );
